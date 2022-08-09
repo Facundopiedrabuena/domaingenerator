a473e94ead11bb9566e29e25d6632393a45cb0ee
Facundopiedrabuena <faculpda46@gmail.com>
2022-08-09 08:02:59 +0000

    mi commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,21 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log(generadorDominio());
 };
+
+function generadorDominio() {
+  var pronoun = ["the", "our"];
+  var adj = ["great", "big"];
+  var noun = ["jogger", "racoon"];
+  var paginaweb = "";
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let a = 0; a < adj.length; a++) {
+      for (let b = 0; b < noun.length; b++) {
+        paginaweb += pronoun[i] + adj[a] + noun[b] + ".com" + " ";
+      }
+    }
+  }
+  return paginaweb;
+}
